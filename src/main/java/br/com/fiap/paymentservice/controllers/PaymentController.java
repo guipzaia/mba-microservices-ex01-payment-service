4809b134a359cc0b5c62ac4ec730cccfaa043e3a
Guilherme Zaia <gui.p.zaia@gmail.com>
2019-04-13 14:33:11 -0300

    Dockerização da API

--- src/main/java/br/com/fiap/paymentservice/controllers/PaymentController.java
+++ src/main/java/br/com/fiap/paymentservice/controllers/PaymentController.java
@@ -6,14 +6,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
 import java.util.ArrayList;
 
 @Slf4j
 @RestController
+@Api(value = "Payment", description = "Payment REST API")
 @RequestMapping("payments")
 public class PaymentController {
 
     @GetMapping()
+    @ApiOperation(httpMethod = "GET", value = "Get all payments")
+    	@ApiResponses(value = {
+    		@ApiResponse(
+                code = 200,
+                message = "Get all payments",
+                response = ArrayList.class
+            )
+        })
     public ResponseEntity<ArrayList<PaymentDTO>> all(){
 
         log.info("All payments");
@@ -24,7 +39,17 @@ public class PaymentController {
     }
 
     @GetMapping("/{uuid}")
-    public ResponseEntity<PaymentDTO> findById(@PathVariable(value = "uuid", required = true) String uuid){
+    @ApiOperation(httpMethod = "GET", value = "Get a payment by Id")
+	@ApiResponses(value = {
+		@ApiResponse(
+            code = 200,
+            message = "Get a payment by Id",
+            response = PaymentDTO.class
+        )
+    })
+    public ResponseEntity<PaymentDTO> findById(
+    		@ApiParam( value = "Payment Id", required = true)
+    		@PathVariable(value = "uuid", required = true) String uuid){
 
         log.info("Load payment {}", uuid);
 
@@ -34,6 +59,14 @@ public class PaymentController {
     }
 
     @PostMapping()
+    @ApiOperation(httpMethod = "POST", value = "Save a payment")
+	@ApiResponses(value = {
+		@ApiResponse(
+            code = 200,
+            message = "Save a payment",
+            response = PaymentDTO.class
+        )
+    })
     public ResponseEntity<PaymentDTO> save(@RequestBody PaymentDTO PaymentDTO){
 
         log.info("Save payment {}", PaymentDTO);
@@ -42,7 +75,17 @@ public class PaymentController {
     }
 
     @PutMapping("/{uuid}")
-    public ResponseEntity<PaymentDTO> update(@PathVariable("uuid") String uuid, @RequestBody PaymentDTO PaymentDTO){
+    @ApiOperation(httpMethod = "PUT", value = "Update a payment by Id")
+		@ApiResponses(value = {
+			@ApiResponse(
+	            code = 200,
+	            message = "Update a payment by Id",
+	            response = PaymentDTO.class
+	        )
+	    })
+    public ResponseEntity<PaymentDTO> update(
+    		@ApiParam( value = "Payment Id", required = true) 
+    		@PathVariable("uuid") String uuid, @RequestBody PaymentDTO PaymentDTO){
 
         log.info("Update payment {} {}", uuid, PaymentDTO);
 
@@ -50,6 +93,14 @@ public class PaymentController {
     }
 
     @DeleteMapping("/{uuid}")
+    @ApiOperation(httpMethod = "DELETE", value = "Delete a payment by Id")
+	@ApiResponses(value = {
+		@ApiResponse(
+            code = 200,
+            message = "Delete a payment by Id",
+            response = PaymentDTO.class
+        )
+    })
     public ResponseEntity<PaymentDTO> delete(@PathVariable("uuid") String uuid){
 
         log.info("Delete payment {}", uuid);
